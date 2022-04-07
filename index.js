9377bfb09f296e3926dde0949c06a5ce44e50031
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-07 12:28:50 +0300

    Constructive and destructive object methods

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Pauline',
+    streetAddress: 'Rainbow Road'
+}
+
+function updateEmployeeWithKeyAndValue(obj,key,value){
+//    const newObj = {...obj};
+//     newObj[key]=value
+//     return newObj
+    return {...obj, [key]:value }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key]=value;
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key, value){
+    const newObj={...obj};
+    newObj[key]=value;
+    delete newObj.key;
+    return newObj
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj,key,value){
+    obj[key]=value;
+    delete obj.key;
+    return obj
+}
